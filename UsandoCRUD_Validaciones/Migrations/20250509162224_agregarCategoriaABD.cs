cceebc70d43cd0d98c3256fe992fc94ac41cb21e
gianc <gianc@DESKTOP-LDKD3DC>
2025-05-26 19:50:25 -0500

    Agregar archivos de proyecto.

---
+++ UsandoCRUD_Validaciones/Migrations/20250509162224_agregarCategoriaABD.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UsandoCRUD_Validaciones.Migrations
+{
+    /// <inheritdoc />
+    public partial class agregarCategoriaABD : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categoria",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NombreCategoria = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Orden = table.Column<int>(type: "int", nullable: false),
+                    Descripcion = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categoria", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Categoria");
+        }
+    }
+}
---
+++ UsandoCRUD_Validaciones/Models/Categoria.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.Contracts;
+
+namespace UsandoCRUD_Validaciones.Models
+{
+    public class Categoria
+    {
+
+        [Key]
+
+        public int Id { get; set; }
+        [Required (ErrorMessage ="El Nombre de categoria es obligatorio")]
+
+        public string? NombreCategoria { get; set; }
+        [Required(ErrorMessage ="El Orden es obligatorio")]
+        [Range(1,int.MaxValue,ErrorMessage ="El orden debe ser mayor a cero")]
+
+        public int Orden {  get; set; }
+        [Required(ErrorMessage ="La descripcion es obligatoria")]
+
+        public string? Descripcion { get; set; }
+    }
+}
